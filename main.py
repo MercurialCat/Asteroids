9283137807ab7549781c9bd197e0d8f1385799f7
MercurialCat <dbtprivate@gmail.com>
2025-02-20 15:29:25 -0500

    C: Added full W,A,S,D player movement and added a GRoup containers to manage game objects.

--- main.py
+++ main.py
@@ -3,26 +3,37 @@ from constants import *
 from player import Player
 
 def main():
-    print("Starting asteroids!")
+    print("Starting asteroids!") #starting parameters
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init() #REMEMBER TO ACTUALLY INIT YOUR MODULES HAHAH
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    fps = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0 
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) #screen size
+    fps = pygame.time.Clock() #setting fps/clock
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable,drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #player size on screen
+    dt = 0 #delta time
 
-    while True:
+
+
+    while True:  #This is the start of the game loop, *where all the *magic* happens
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updateable.update(dt)
+
         screen.fill(("black"))
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
-    
-        dt = fps.tick(60) / 1000
+
+        dt = fps.tick(60) / 1000 # framerate set to 60
+       
 
 
 
